6dbfc58384fd2c47e2b64b150bf97fe8965bba82
Sue Smith <sue@benormal.info>
2023-02-07 17:40:10 +0000

    Add source code from Fiddle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-/// <reference types="@fastly/js-compute" />
+addEventListener("fetch", event => {
+  // Get the request from the client.
+  const req = event.request;
 
-addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
+  // Send the request to `origin_0`.
+  const backendResponse = fetch(req, {
+    backend: "origin_0"
+  });
 
-async function handleRequest(event) {
-  return new Response("OK", { status: 200 });
-}
+  // Send the backend response back to the client.
+  event.respondWith(backendResponse);
+
+  // This is a minimal Compute@Edge app in JavaScript.  For alternative starter
+  // code for your fiddle, see the code examples on our developer hub:
+  // https://developer.fastly.com/solutions/examples/javascript/
+});
